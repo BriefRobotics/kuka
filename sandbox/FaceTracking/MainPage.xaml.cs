f0770b708a2f65fd3d0e13c7bc5348c0c6f334b0
AshleyF <ashley.feniello@live.com>
2018-04-05 11:14:28 -0700

    Face tracking sample

---
+++ sandbox/FaceTracking/MainPage.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Shapes;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Core;
+
+namespace FaceTracking
+{
+    public sealed partial class MainPage : Page
+    {
+        private readonly SolidColorBrush fill = new SolidColorBrush(Colors.Transparent);
+        private readonly SolidColorBrush red = new SolidColorBrush(Colors.Red);
+        private readonly SolidColorBrush yellow = new SolidColorBrush(Colors.Yellow);
+
+        private Tracker tracker;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            tracker = new Tracker(Display);
+            tracker.Detected += (_, f) =>
+            {
+                var faces = f.Item1;
+                var w = f.Item2;
+                var h = f.Item3;
+                this.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+                {
+                    Canvas.Children.Clear();
+
+                    double actualWidth = this.Canvas.ActualWidth;
+                    double actualHeight = this.Canvas.ActualHeight;
+
+                    double widthScale = w / actualWidth;
+                    double heightScale = h / actualHeight;
+
+                    var nearest = 0u;
+                    foreach (var face in faces)
+                    {
+                        var box = face.FaceBox;
+                        var area = box.Width * box.Height;
+                        if (area > nearest)
+                        {
+                            nearest = area;
+                        }
+                    }
+
+                    foreach (var face in faces)
+                    {
+                        var fb = face.FaceBox;
+                        Rectangle box = new Rectangle();
+                        box.Width = (uint)(fb.Width / widthScale);
+                        box.Height = (uint)(fb.Height / heightScale);
+                        box.Fill = fill;
+                        box.Stroke = fb.Width * fb.Height == nearest ? red : yellow;
+                        box.StrokeThickness = 4.0;
+                        box.Margin = new Thickness((uint)(face.FaceBox.X / widthScale), (uint)(face.FaceBox.Y / heightScale), 0, 0);
+                        this.Canvas.Children.Add(box);
+                    }
+                }).AsTask().Wait();
+            };
+            tracker.Start(TimeSpan.FromMilliseconds(66));
+        }
+    }
+}
---
+++ sandbox/FaceTracking/Tracker.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Threading;
+using Windows.System.Threading;
+using Windows.Media;
+using Windows.Media.FaceAnalysis;
+using Windows.Media.MediaProperties;
+using Windows.Media.Devices;
+using Windows.Media.Capture;
+using Windows.Graphics.Imaging;
+using Windows.UI.Xaml.Controls;
+using System.Collections.Generic;
+
+namespace FaceTracking
+{
+    public class Tracker
+    {
+        private readonly CaptureElement display;
+        private readonly MediaCapture cam = new MediaCapture();
+        private readonly SemaphoreSlim semaphore = new SemaphoreSlim(1);
+
+        private FaceTracker tracker;
+        private VideoEncodingProperties properties;
+        private ThreadPoolTimer timer;
+
+        public event EventHandler<Tuple<IList<DetectedFace>, int, int>> Detected;
+
+        public Tracker(CaptureElement display)
+        {
+            this.display = display;
+        }
+
+        private async void Frame(ThreadPoolTimer timer)
+        {
+            if (cam.CameraStreamState != CameraStreamState.Streaming) return;
+            if (!semaphore.Wait(0)) return;
+
+            try
+            {
+                const BitmapPixelFormat InputPixelFormat = BitmapPixelFormat.Nv12;
+                using (VideoFrame frame = new VideoFrame(InputPixelFormat, (int)properties.Width, (int)properties.Height))
+                {
+                    await cam.GetPreviewFrameAsync(frame);
+                    var faces = await tracker.ProcessNextFrameAsync(frame);
+                    var b = frame.SoftwareBitmap;
+                    Detected?.Invoke(this, Tuple.Create(faces, b.PixelWidth, b.PixelHeight));
+                }
+            }
+            finally
+            {
+                semaphore.Release();
+            }
+        }
+
+        public async void Start(TimeSpan rate)
+        {
+            tracker = await FaceTracker.CreateAsync();
+            cam.Failed += (_, e) => throw new Exception($"Camera failure: {e.Message}");
+            await cam.InitializeAsync(new MediaCaptureInitializationSettings() { StreamingCaptureMode = StreamingCaptureMode.Video });
+            properties = cam.VideoDeviceController.GetMediaStreamProperties(MediaStreamType.VideoPreview) as VideoEncodingProperties;
+            if (display != null) display.Source = cam;
+            await cam.StartPreviewAsync();
+            timer = ThreadPoolTimer.CreatePeriodicTimer(new TimerElapsedHandler(Frame), rate);
+        }
+
+        public async void Stop()
+        {
+            timer?.Cancel();
+            await cam.StopPreviewAsync();
+        }
+    }
+}
